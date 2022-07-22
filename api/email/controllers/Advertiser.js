c329f23fc6a674847cbbb81e6e263a42c5e035c6
plutusdotbtc <97111019+plutusdotbtc@users.noreply.github.com>
2022-07-22 13:05:37 +0200

    Update Advertiser.js

--- api/email/controllers/Advertiser.js
+++ api/email/controllers/Advertiser.js
@@ -5,7 +5,7 @@ module.exports = {
           to: ctx.request.body.email,
           from: "Daniel Fritsche <daniel@northwestarkansasdaily.com>",
           cc: ["luke@northwestarkansasdaily.com"],
-          replyTo: "luke@northwestarkansasdaily.com",
+          replyTo: "daniel@northwestarkansasdaily.com",
           subject: "Thanks for reaching out!",
           html: `<p style="font-family:Helvetica;line-height:20px;margin-top:0px;color:rgb(32,32,32);font-size:15px">Hi Friend,</p><p style="font-family:Helvetica;line-height:20px;margin-top:0px;color:rgb(32,32,32);font-size:15px">I saw that you inquired about partnering with NWA Daily for advertising. We would love to hear more about your company or organization and how we could help if you have time this week for a quick call?</p><p style="font-family:Helvetica;line-height:20px;margin-top:0px;color:rgb(32,32,32);font-size:15px">If so we'd love to chat with you and answer any questions you might have. You can pick a time on our <a href="https://calendly.com/d/cj7-726-4mp/nwa-daily-advertising">calendar here!</a></p><p style="font-family:Helvetica;line-height:20px;margin-top:0px;color:rgb(32,32,32);font-size:15px">Look forward to speaking with you!</p><p style="font-family:Helvetica;line-height:20px;margin-top:0px;color:rgb(32,32,32);font-size:15px">Daniel Fritsche</p>`
         });
